ac8c95754c504b7db8452ab3011b80ed9d8c089c
jincai822 <838333241@qq.com>
2026-08-09 10:28:17 +0800

    feat: add support for Mirabox N3 (6602:1000)

--- src/mappings.rs
+++ src/mappings.rs
@@ -41,6 +41,7 @@ pub const AKP03R_PID: u16 = 0x1003;
 pub const AKP03E_REV2_PID: u16 = 0x3002;
 pub const AKP03R_REV2_PID: u16 = 0x3003;
 pub const N3_PID: u16 = 0x1002;
+pub const N3_1000_PID: u16 = 0x1000;
 pub const N3EN_PID: u16 = 0x1003;
 pub const N3CN3_PID: u16 = 0x1002;
 pub const SOOMFON_SE_PID: u16 = 0x3001;
@@ -55,6 +56,7 @@ pub const AKP03R_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, AJAZZ_VID, AKP0
 pub const AKP03E_REV2_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, AJAZZ_VID, AKP03E_REV2_PID);
 pub const AKP03R_REV2_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, AJAZZ_VID, AKP03R_REV2_PID);
 pub const N3_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, N3_VID, N3_PID);
+pub const N3_1000_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, N3_VID, N3_1000_PID);
 pub const N3EN_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MIRABOX_VID, N3EN_PID);
 pub const N3CN3_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, MIRABOX_VID, N3CN3_PID);
 pub const SOOMFON_SE_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, SOOMFON_VID, SOOMFON_SE_PID);
@@ -63,13 +65,14 @@ pub const TREASLIN_QUERY: DeviceQuery = DeviceQuery::new(65440, 1, TREASLIN_VID,
 pub const REDRAGON_SS551_QUERY: DeviceQuery =
     DeviceQuery::new(65440, 1, REDRAGON_VID, REDRAGON_SS551_PID);
 
-pub const QUERIES: [DeviceQuery; 12] = [
+pub const QUERIES: [DeviceQuery; 13] = [
     AKP03_QUERY,
     AKP03E_QUERY,
     AKP03R_QUERY,
     AKP03E_REV2_QUERY,
     AKP03R_REV2_QUERY,
     N3_QUERY,
+    N3_1000_QUERY,
     N3EN_QUERY,
     N3CN3_QUERY,
     SOOMFON_SE_QUERY,
@@ -93,6 +96,7 @@ impl Kind {
 
             N3_VID => match pid {
                 N3_PID => Some(Kind::N3),
+                N3_1000_PID => Some(Kind::N3),
                 _ => None,
             },
 
